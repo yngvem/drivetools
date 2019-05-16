e28340359e4bb0663fc60b18ad8736302ffc9f56
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2019-05-16 07:34:57 +0200

    Added verbosity flag

--- drivesync.py
+++ drivesync.py
@@ -7,8 +7,9 @@ if __name__ == '__main__':
     parser.add_argument('local_path')
     parser.add_argument('remote_path')
     parser.add_argument('credentials_file')
+    parser.add_argument('--verbose', type=bool, default=False)
 
     args = parser.parse_args()
 
     drive = drivetools.start_gdrive(args.credentials_file)
-    drivetools.sync_folder(drive, args.local_path, args.remote_path)
+    drivetools.sync_folder(drive, args.local_path, args.remote_path, args.verbose)
